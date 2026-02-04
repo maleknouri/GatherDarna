7de694db9c3600163e450fbc69c9f7f282f2ef2d
maleknouri <malek.nouri96@gmail.com>
2026-02-04 12:51:01 +0100

    First Iteration of our office map

---
+++ Room_Builder_48x48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Room_Builder_48x48" tilewidth="32" tileheight="32" tilecount="19266" columns="114">
+ <image source="../../../Desktop/New folder (2)/Gather/Darna assets/Darna assets/interior/moderninteriors-win/1_Interiors/48x48/Room_Builder_48x48.png" width="3648" height="5424"/>
+</tileset>
